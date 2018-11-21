8c918b65cce11f608de699f86b0bc882e3ab91d3
JoeLametta <JoeLametta@users.noreply.github.com>
2018-11-21 08:00:00 +0000

    Drop whipper caching [12/?]

--- whipper/command/cd.py
+++ whipper/command/cd.py
@@ -461,8 +461,6 @@ Log files will log the path to tracks relative to this directory.
                 self.itable.setFile(number, 1, trackResult.filename,
                                     self.ittoc.getTrackLength(number), number)
 
-            self.program.saveRipResult()
-
         # check for hidden track one audio
         htoa = self.program.getHTOA()
         if htoa:
@@ -492,8 +490,6 @@ Log files will log the path to tracks relative to this directory.
             print('AccurateRip entry not found')
 
         accurip.print_report(self.program.result)
-
-        self.program.saveRipResult()
 
         self.program.writeLog(discName, self.logger)
 
